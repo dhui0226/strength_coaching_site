c1e197cd4c36e7194375177f2268fa2a5d913a80
dhui <dhui0226@gmail.com>
2021-08-29 23:32:46 -0500

    text is over bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import Nav from '../components/nav'
 import {
   pageStyles,
   mission,
-  headingStyles,
+  imageContainer,
   paragraphStyles,
   quoteStyles,
   btnContainer,
@@ -18,13 +19,22 @@ const IndexPage = () => {
     <main className={pageStyles}>
       <title>Home Page</title>
       <Nav></Nav>
+      <div className={imageContainer}>
+        <StaticImage
+          alt='deadlift'
+          src='../images/deadlift.jpeg'
+          
+        />
+        <p>Powerlifting Online Coaching</p>
+      </div>
+
       <div className={mission}>
-        <h1 className={headingStyles}>Powerlifting Online Coaching</h1>
         <p className={paragraphStyles}>Infamous Crew is about creating a sense of community within a sport that has done so much for us.</p>
         <p className={paragraphStyles}>We believe that having a team to push you to your limits will make you stronger.</p>
         <p className={paragraphStyles}>That's why our priority has always been to build up a family of highly driven individuals that can rely on each other.</p>
         <h2 className={quoteStyles}>"Strength in Unity"</h2>
       </div>
+
       <div className={btnContainer}>
         <Link to='/application'>
           <button className={btn}>
