b489e54c0facf8e7318eba426573da79e900220d
dhui <dhui0226@gmail.com>
2021-09-08 19:11:38 -0500

    form submissions work

--- src/pages/application.js
+++ src/pages/application.js
@@ -44,7 +44,7 @@ const ApplicationPage = () => {
             </div>  
 
             <div className={pageContainer}>
-                <form className={formContainer} name='application' method='post' data-netlify='true' data-netlify-honeypot='bot-field'>
+                <form className={formContainer} name='application' method='post' data-netlify='true' data-netlify-honeypot='bot-field' onSubmit='submit'>
                     <input type='hidden' name='form-name' value='application' />
                     <p>Please fill out the section below, as honestly and completely as you can to give us more information on how we can work together.</p>
                     <div className={nameContainer}>
@@ -52,6 +52,7 @@ const ApplicationPage = () => {
                             <span>First Name</span>
                             <input
                             className={firstNameInput}
+                            name='firstName'
                             type='text'
                             value={firstName}
                             onChange={(event) => {setFirstName(event.target.value)}}></input>
@@ -60,6 +61,7 @@ const ApplicationPage = () => {
                             <span className={lastNameLabel}>Last Name</span>
                             <input 
                             className={lastNameInput}
+                            name='lastName'
                             type='text'
                             value={lastName}
                             onChange={(event) => {setLastName(event.target.value)}}></input>       
@@ -68,6 +70,7 @@ const ApplicationPage = () => {
                     <label className={form}>
                         <span>Email Address</span>
                         <input 
+                        name='email'
                         type='email'
                         value={email}
                         onChange={(event) => {setEmail(event.target.value)}}></input>
@@ -75,6 +78,7 @@ const ApplicationPage = () => {
                     <label className={form}>
                         <span>Date of Birth</span>
                         <input 
+                        name='dob'
                         type='date'
                         value={dob}
                         onChange={(event) => {setDOB(event.target.value)}}></input>
@@ -83,6 +87,7 @@ const ApplicationPage = () => {
                         <span>Gender</span>
                         <select 
                         className={genderSelect} 
+                        name='gender'
                         onChange={(event) => {setGender(event.target.value)}}>
                             {genderList.map((selectedGender) => 
                                 <option value={selectedGender} key={selectedGender}>{selectedGender}</option>
@@ -93,6 +98,7 @@ const ApplicationPage = () => {
                         <span>Height</span>
                         <input 
                         className={smallerInput}
+                        name='height'
                         type='number'
                         value={height}
                         onChange={(event) => {setHeight(event.target.value)}}></input>
@@ -101,6 +107,7 @@ const ApplicationPage = () => {
                         <span>Weight</span>
                         <input 
                         className={smallerInput}
+                        name='weight'
                         type='number'
                         value={weight}
                         onChange={(event) => {setWeight(event.target.value)}}></input>
@@ -109,6 +116,7 @@ const ApplicationPage = () => {
                         <span>Years Training / Athletic Background</span>
                         <input 
                         className={biggerInput}
+                        name='detailsOne'
                         type='text'
                         value={detailsOne}
                         onChange={(event) => {setDetailsOne(event.target.value)}}></input>
@@ -118,6 +126,7 @@ const ApplicationPage = () => {
                         <span className={description}>How often are you performing the Squat,Bench Press, and deadlift and its variations during a training week?</span>
                         <input 
                         className={biggerInput}
+                        name='detailsTwo'
                         type='text'
                         value={detailsTwo}
                         onChange={(event) => {setDetailsTwo(event.target.value)}}></input>
@@ -127,6 +136,7 @@ const ApplicationPage = () => {
                         If so, please leave details below</span>
                         <input 
                         className={biggerInput}
+                        name='detailsThree'
                         type='text'
                         value={detailsThree}
                         onChange={(event) => {setDetailsThree(event.target.value)}}></input>
@@ -135,14 +145,25 @@ const ApplicationPage = () => {
                         <span>Current 1 Rep Max in S/B/D</span>
                         <input 
                         className={biggerInput}
+                        name='sbdMaxes'
                         type='text'
                         value={sbdMaxes}
                         onChange={(event) => {setSbdMaxes(event.target.value)}}></input>
                     </label>
+                    <div className={btnContainer}>
+                        <button
+                        className={btn}
+                        type='submit'
+                        onClick={() => {
+                            handleSubmit()
+                        }}>
+                            <p>Submit</p>
+                        </button>
+                    </div>
                 </form>
             </div>
 
-            <div className={btnContainer}>
+            {/*<div className={btnContainer}>
                 <button
                 className={btn}
                 type='submit'
@@ -151,7 +172,7 @@ const ApplicationPage = () => {
                 }}>
                     <p>Submit</p>
                 </button>
-            </div>
+            </div>*/}
         </Layout>
     )
 }
